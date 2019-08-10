47f4b2daae9320fc8fc357a08b3868a66fe48b0a
Aggie M <43214029+BigFatPony@users.noreply.github.com>
2019-08-10 20:18:43 +0200

    Gender chosing
    Basic use of classes

--- Main.py
+++ Main.py
@@ -2,5 +2,16 @@ import random
 from random import randint
 
 print("Welcome  Aggie's Life Simulator \n"
-      "All these events are randoml generated n"
+      "All these events are randomly generated \n"
       " \n")
+
+class Player:
+    gender = (randint(1,2))
+    
+if (Player.gender) == 1:
+    print("You have been born a male")
+    Player.gender = 1
+
+else:
+    print("You have been born a female")
+    Player.gender = 2
